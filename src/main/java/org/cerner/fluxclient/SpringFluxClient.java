47cda39cccc5f817f1862ec8648636cf741f096d
Ishani Ray <ishani_ray@yahoo.co.in>
2020-04-10 16:11:40 +0530

    Adding logger.

--- src/main/java/org/cerner/fluxclient/SpringFluxClient.java
+++ src/main/java/org/cerner/fluxclient/SpringFluxClient.java
@@ -2,12 +2,15 @@ package org.cerner.fluxclient;
 
 import org.cerner.fluxclient.subscriber.EmployeeSubscriber;
 import org.cerner.model.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
 
 public class SpringFluxClient {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringFluxClient.class);
 
 	public void getStream() {
 		WebClient client = WebClient.create("http://localhost:8081");
@@ -17,20 +20,19 @@ public class SpringFluxClient {
 		Flux<Employee> employeeStream = client.get().uri("/").retrieve().bodyToFlux(type);
 
 		// Approach 1: Lambda Consumers - onNext, onError, onComplete
-		employeeStream.subscribe(employee -> System.out.println(employee.toString()),
-				error -> System.out.println("An error occured: " + error),
-				() -> System.out.println("This stream has now ended."));
+		employeeStream.subscribe(employee -> LOGGER.info("Received: " + employee.toString()),
+				error -> LOGGER.error("An error occured: " + error), () -> LOGGER.info("End of stream."));
 
 		// Approach 2: Approach 1 with Backpressure and Backpressure Buffer
-		employeeStream.onBackpressureBuffer().subscribe(employee -> System.out.println(employee.toString()),
-				error -> System.out.println("An error occured: " + error),
-				() -> System.out.println("This stream has now ended."), subscription -> subscription.request(3));
+		employeeStream.onBackpressureBuffer().subscribe(employee -> LOGGER.info("Received: " + employee.toString()),
+				error -> LOGGER.error("An error occured: " + error), () -> LOGGER.info("End of stream."),
+				subscription -> subscription.request(3));
 
 		// Approach 3: Writing a subscriber class (extend BaseSubscriber)
 		employeeStream.subscribe(new EmployeeSubscriber<Employee>());
 	}
 
 	public void doSomething() {
-		System.out.println("Doing something else...");
+		LOGGER.debug("Doing something else...");
 	}
 }
--- src/main/java/org/cerner/fluxclient/subscriber/EmployeeSubscriber.java
+++ src/main/java/org/cerner/fluxclient/subscriber/EmployeeSubscriber.java
@@ -1,10 +1,13 @@
 package org.cerner.fluxclient.subscriber;
 
 import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import reactor.core.publisher.BaseSubscriber;
 
 public class EmployeeSubscriber<Employee> extends BaseSubscriber<Employee> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeSubscriber.class);
 
 	int consumed = 0;
 	final int limit = 2;
@@ -16,7 +19,7 @@ public class EmployeeSubscriber<Employee> extends BaseSubscriber<Employee> {
 
 	@Override
 	public void hookOnNext(Employee employee) {
-		System.out.println(employee.toString());
+		LOGGER.info("Received: " + employee.toString());
 
 		consumed++;
 
@@ -29,11 +32,11 @@ public class EmployeeSubscriber<Employee> extends BaseSubscriber<Employee> {
 
 	@Override
 	public void hookOnError(Throwable error) {
-		System.out.println("An error occured: " + error);
+		LOGGER.error("An error occured: " + error);
 	}
 
 	@Override
 	public void hookOnComplete() {
-		System.out.println("This stream has now ended.");
+		LOGGER.info("End of stream.");
 	}
 }
